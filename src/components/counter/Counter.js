64ed1887bf934d8ede1c77a5cdc8e8572192cb78
balamanohar96 <balamanohar.yeminedi@gmail.com>
2023-06-15 14:36:05 +0530

    dfd

--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./counter.css"
+import "./counter.css";
 
 function Counter() {
   let [count, setCount] = useState(0);
@@ -7,13 +7,14 @@ function Counter() {
     <>
       <div className="first">
         <p className="countPara">COUNTER</p>
-        <h3 className="count"><span className="vvv">Count : </span>{count}</h3>
-        <button onClick={()=>setCount(count+1 )}>incriment</button>
-        <button onClick={()=>setCount(count-1)}>decrement</button>
-        <button onClick={()=>setCount(0)}>reset</button>
+        <h3 className="count">
+          <span className="vvv">Count : </span>
+          {count}
+        </h3>
+        <button onClick={() => setCount(count + 1)}>incriment</button>
+        <button onClick={() => setCount(count - 1)}>decrement</button>
+        <button onClick={() => setCount(0)}>reset</button>
       </div>
-
-      {/* <Link to="/">Home</Link> */}
     </>
   );
 }
